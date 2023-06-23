023cd489c732fb2edc82491a6e522fac975f080f
SpyrosAlvanakis <s.alvanakis@gmail.com>
2023-06-23 14:00:28 +0300

    GENEFU

--- R_time/Genefu.r
+++ R_time/Genefu.r
@@ -1,11 +1,8 @@
 # if (!require("BiocManager", quietly = TRUE))
 #   install.packages("BiocManager")
 # BiocManager::install("genefu")
-# install.packages("pamr")
-
 
 library(genefu)
-library(pamr)
 
 rm(list = ls())
 
@@ -26,29 +23,12 @@ for (gene in genes$GeneID) {
 colnames(annot_final) = 'EntrezGene.ID'
 annot_final = data.matrix(annot_final)
 
-
-data(scmod1.robust)
-data(sig.genius)
-# Compute GENIUS risk scores
-genius_result <- genius(data = expr_matrix, annot = annot_final, do.mapping = TRUE, do.scale = FALSE)
-
-# Access the risk scores for different subtypes
-risk_score_ER_HER2_neg <- genius_result$GENIUSM1  # ER-/HER2- subtype
-risk_score_HER2_pos <- genius_result$GENIUSM2     # HER2+ subtype
-risk_score_ER_HER2_neg <- genius_result$GENIUSM3  # ER+/HER2- subtype
-
-# Print the risk scores
-cat("ER-/HER2- Risk Score:", risk_score_ER_HER2_neg, "\n")
-cat("HER2+ Risk Score:", risk_score_HER2_pos, "\n")
-cat("ER+/HER2- Risk Score:", risk_score_ER_HER2_neg, "\n")
-
-genius_df <- data.frame(ER_HER2_neg = genius_result$GENIUSM1,
-                        HER2_pos = genius_result$GENIUSM2,
-                        er_pos_HER2_neg = genius_result$GENIUSM3,
-                        score = genius_result$score)
-
-# Export the data frame to a CSV file
-write.csv(genius_df, file = "genius_results.csv", row.names = FALSE)
+data(mod1)
+scmod1.expos <- subtype.cluster(module.ESR1=mod1$ESR1, module.ERBB2=mod1$ERBB2,
+                                module.AURKA=mod1$AURKA, data=expr_matrix, annot=annot_final, do.mapping=TRUE,
+                                do.scale=TRUE, plot=FALSE, verbose=TRUE)
+results = data.frame(scmod1.expos[["subtype.proba"]])
+write.csv(results, file = "scmod1.csv", row.names = TRUE)
 
 
 
